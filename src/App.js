e3acbea5457d1e6a5c4c5b0a9d3ac9cb444d3e4d
unknown <desiredshellstate@gmail.com>
2022-09-20 09:47:47 +0200

    Rendered Weather Api no styling done yet or page layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
+import WeatherApi from './components/WeatherApi';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <WeatherApi />
     </div>
   );
 }
---
+++ src/components/WeatherApi.jsx
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from "react"
+
+const apiKey = "003511fe490763d6e3729d6e0090f05f";
+const cityName = "germany";
+const metric = "units=metric";
+const url = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&APPID=${apiKey}&${metric}`;
+
+
+const WeatherApi = () => {
+  const [data, setData] = useState([])
+  function fetchData() {
+    fetch(url)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        setData([data]);
+      });
+  }
+
+  useEffect(function () {
+    fetchData();
+  }, [])
+
+  return (
+    <div>
+      {data.map((data, index) => {
+        return (
+          <div key={index}>
+            <h2>Location: {data.name}</h2>
+            <h2>Weather: {data.weather[0].main}</h2>
+            <h2>Temp: {data.main.temp}</h2>
+            <h2>Longitude: {data.coord.lon}</h2>
+            <h2>Latitude: {data.coord.lat}</h2>
+            <h2>Feels Like: {data.main.feels_like}</h2>
+            <h2>Pressure: {data.main.pressure}</h2>
+            <h2>Humidity: {data.main.humidity}</h2>
+            <h2>Minimum Temp: {data.main.temp_min}</h2>
+            <h2>Max Temp: {data.main.temp_max}</h2>
+            <h2>Visibility: {data.visibility}</h2>
+            <h2>Wind Speed: {data.wind.speed}</h2>
+            <h2>Timezone: {data.timezone}</h2>
+            <hr />
+          </div>
+        );
+      })}
+    </div>
+  )
+}
+
+
+export default WeatherApi;
